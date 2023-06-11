229a839a263ed86596611a39c63ea087081a63a8
bavshehata <bavshehata@gmail.com>
2023-06-11 06:00:21 +0300

    prevent recursive multiprcoessing

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
 # @app.route("/predict")
 # def hello_world(name):
 #     return f"Hello, {escape(name)}!"  
